6d5f28e1232b07215fb43657a0014186e8540652
Fikrat.Gasimov <qasimov.fikrat@gmail.com>
2020-02-07 11:47:42 +0100

    First Commit

---
+++ dataloaders/datasets/apolloscape.py
@@ -0,0 +1,187 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from modeling import deeplab
+from modeling import aspp
+from modeling.aspp import _ASPPModule, ASPP
+from modeling.deeplab import DeepLab
+from doc import  deeplab_xception
+from modeling.backbone import xception
+from mypath import Path
+import json
+from utils import calculate_weights,loss,lr_scheduler,metrics,saver,summaries
+from tqdm import trange
+import os
+from pycocotools.coco import COCO
+from pycocotools import mask
+import pycocotools._mask as _mask
+from torchvision import transforms
+from dataloaders import custom_transforms as tr
+from PIL import Image, ImageFile
+from mypath import Path
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+class COCOSegmentation(Dataset):
+    NUM_CLASSES = 38
+    CAT_LIST = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31, 32, 33, 34, 255, 255]
+
+    def __init__(self,
+                 args,
+                 base_dir=Path.db_root_dir('coco'),
+                 split='train',
+                 year='2017'):
+        super().__init__()
+        # Annotations && Instances path via Json
+        ann_file = os.path.join(base_dir, 'annotations/instances_{}{}.json'.format(split, year))
+
+        # pth file is great alternative to impart PythonPath Environment Variables
+        # Annotations && Ids files PTH
+        ids_file = os.path.join(base_dir, 'annotations/{}_ids_{}.pth'.format(split, year))
+
+        # images directory containing training_set - > 2017
+        self.img_dir = os.path.join(base_dir, 'images/{}{}'.format(split, year))
+
+        # Split in this respect, train set
+        self.split = split
+
+        # Annotation file
+        self.coco = COCO(ann_file)
+        self.coco_mask = mask
+
+        if os.path.exists(ids_file):
+            self.ids = torch.load(ids_file)
+
+        else:
+            ids = list(self.coco.imgs.keys())
+            self.ids = self._preprocess(ids, ids_file)
+        self.args = args
+
+    def __getitem__(self, index):
+        _img, _target = self._make_img_gt_point_pair(index)
+
+        sample = {'image': _img, 'label': _target}
+
+        #### Train Set ####
+        if self.split == "train":
+            return self.transform_tr(sample)
+
+       ###### validation set ####
+        elif self.split == 'val':
+            return self.transform_val(sample)
+
+    def _make_img_gt_point_pair(self, index):
+        coco = self.coco
+        img_id = self.ids[index]
+        img_metadata = coco.loadImgs(img_id)[0]
+        path = img_metadata['file_name']
+        _img = Image.open(os.path.join(self.img_dir, path)).convert('RGB')
+        cocotarget = coco.loadAnns(coco.getAnnIds(imgIds=img_id))
+        _target = Image.fromarray(self._gen_seg_mask(
+            cocotarget, img_metadata['height'], img_metadata['width']))
+
+        return _img, _target
+
+    def _preprocess(self, ids, ids_file):
+        print("Preprocessing mask, this will take a while. " +
+              "But don't worry, it only run once for each split.")
+        tbar = trange(len(ids))
+        new_ids = []
+        for i in tbar:
+            img_id = ids[i]
+            cocotarget = self.coco.loadAnns(self.coco.getAnnIds(imgIds=img_id))
+            img_metadata = self.coco.loadImgs(img_id)[0]
+            mask = self._gen_seg_mask(cocotarget, img_metadata['height'],
+                                      img_metadata['width'])
+            # more than 1k pixels
+            if (mask > 0).sum() > 1000:
+                new_ids.append(img_id)
+            tbar.set_description('Doing: {}/{}, got {} qualified images'. \
+                                 format(i, len(ids), len(new_ids)))
+        print('Found number of qualified images: ', len(new_ids))
+        torch.save(new_ids, ids_file)
+        return new_ids
+
+    def _gen_seg_mask(self, target, h, w):
+        mask = np.zeros((h, w), dtype=np.uint8)
+        coco_mask = self.coco_mask
+        for instance in target:
+            rle = coco_mask.frPyObjects(instance['segmentation'], h, w)
+            m = coco_mask.decode(rle)
+            cat = instance['category_id']
+            if cat in self.CAT_LIST:
+                c = self.CAT_LIST.index(cat)
+            else:
+                continue
+            if len(m.shape) < 3:
+                mask[:, :] += (mask == 0) * (m * c)
+            else:
+                mask[:, :] += (mask == 0) * (((np.sum(m, axis=2)) > 0) * c).astype(np.uint8)
+        return mask
+    # train transform
+    def transform_tr(self, sample):
+        composed_transforms = transforms.Compose([
+            tr.RandomHorizontalFlip(),
+            tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
+            tr.RandomGaussianBlur(),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+
+        return composed_transforms(sample)
+    # validate transform
+    def transform_val(self, sample):
+
+        composed_transforms = transforms.Compose([
+            tr.FixScaleCrop(crop_size=self.args.crop_size),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+
+        return composed_transforms(sample)
+
+
+    def __len__(self):
+        return len(self.ids)
+
+
+
+if __name__ == "__main__":
+    from dataloaders import custom_transforms as tr
+    from dataloaders.utils import decode_segmap
+    from torch.utils.data import DataLoader
+    from torchvision import transforms
+    import matplotlib.pyplot as plt
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.base_size = 513
+    args.crop_size = 513
+
+    coco_val = COCOSegmentation(args, split='val', year='2017')
+
+    dataloader = DataLoader(coco_val, batch_size=4, shuffle=True, num_workers=0)
+
+    for ii, sample in enumerate(dataloader):
+        for jj in range(sample["image"].size()[0]):
+            img = sample['image'].numpy()
+            gt = sample['label'].numpy()
+            tmp = np.array(gt[jj]).astype(np.uint8)
+            segmap = decode_segmap(tmp, dataset='coco')
+            img_tmp = np.transpose(img[jj], axes=[1, 2, 0])
+            img_tmp *= (0.229, 0.224, 0.225)
+            img_tmp += (0.485, 0.456, 0.406)
+            img_tmp *= 255.0
+            img_tmp = img_tmp.astype(np.uint8)
+            plt.figure()
+            plt.title('display')
+            plt.subplot(211)
+            plt.imshow(img_tmp)
+            plt.subplot(212)
+            plt.imshow(segmap)
+
+        if ii == 1:
+            break
+
+    plt.show(block=True)
