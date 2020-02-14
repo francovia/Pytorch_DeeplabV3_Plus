51fd070efa19c699dbd340c844c1d135fd2c9ffd
fikratgasimov <56602465+fikratgasimov@users.noreply.github.com>
2020-02-14 17:40:32 +0100

    Update __init__.py

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -36,7 +36,14 @@ def make_data_loader(args, **kwargs):
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = None
         return train_loader, val_loader, test_loader, num_class
-
+    elif args.dataset == 'ApolloScape' :
+        train_set == apolloscape.ApolloScapeSegmentation(args, split='train')
+        val_set == apolloscape.ApolloScapeSegmentation(args, split='val')
+        num_class = train_set.NUM_CLASSES
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, suffle=True, **kwargs)
+        val_loader = Dataloader(val_set,batch_size=args.batch_size, suffle=False, **kwargs)
+        test_loader = Dataloader(test_set, batch_size=args.batch_size, suffle=False, **kwargs)
+        return train_loader, val_loader, test_loader, num_class
     else:
         raise NotImplementedError
 
